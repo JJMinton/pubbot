9da1e656db9359309ed965949455a8fa41206553
Ettie Unwin <hjtu2@cam.ac.uk>
2018-03-30 23:22:22 +0100

    Adds oo files

---
+++ bot.py
@@ -0,0 +1,54 @@
+import time
+import event
+import os
+from slackclient import SlackClient
+
+import config
+
+class Bot(object):
+    def __init__(self):
+        self.slack_client = SlackClient(config.bot_user_oauth_token)
+        print(config.bot_user_oauth_token)
+        self.bot_name = "starterbot"
+        self.bot_id = self.get_bot_id()
+
+        if self.bot_id is None:
+            exit("Error, could not find " + self.bot_name)
+
+        self.event = event.Event(self)
+        self.listen()
+
+    def get_bot_id(self):
+        api_call = self.slack_client.api_call("users.list")
+        if api_call.get('ok'):
+            # retrieve all users so we can find our bot
+            users = api_call.get('members')
+            for user in users:
+                if 'name' in user and user.get('name') == self.bot_name:
+                    return "<@" + user.get('id') + ">"
+                else:
+                   print("No id")
+                   return None
+
+    def listen(self):
+        if self.slack_client.rtm_connect(with_team_state=False):
+            print ("Successfully connected, listening for commands")
+            while True:
+                self.event.wait_for_event()
+                time.sleep(1)
+        else:
+            exit("Error, Connection Failed")
+
+
+def get_users():
+    # TODO: Filter out bots
+    slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        return users
+    else:
+        print("Failed to get users")
+        print(api_call.keys())
+        return []
---
+++ command.py
@@ -0,0 +1,40 @@
+import bot
+class Command(object):
+    def __init__(self, sc):
+        self.sc = sc
+        self.commands = {
+            "pub" : self.pub,
+            "help" : self.help
+        }
+
+    def handle_command(self, user, command):
+        response = ""
+
+        if command in self.commands:
+            response += self.commands[command]()
+        else:
+            response += "Sorry I don't understand the command"
+
+        return response
+
+    def pub(self):
+        print("Triggered pub")
+        for user in bot.get_users():
+            print(f"Username: {user.get('name')} with ID: {user.get('id')}")
+            channel = self.sc.api_call("conversations.open",
+                                       users=[user.get('id')])
+            if channel['ok']==True:
+                channelid=channel['channel']['id']
+                print(channelid)
+                self.sc.api_call("chat.postMessage",
+                                 channel=channelid,
+                                 text="Fancy a pint?",
+                                 as_user=True)
+            else:
+                print(f"Status is not ok for user: {user.get('id')}")
+        return ""
+
+    def help(self):
+        response = "Currently I do xyz"
+
+        return response
